fd09d3c7b419ede3680647333dbd62199cc74900
dbeck88 <dbeck88@gmail.com>
2016-12-01 12:06:11 -0700

    include hdr.md
    header file

---
+++ hdr template.h
@@ -0,0 +1,5 @@
+/***************************************************************************
+*  Module: 
+*  Author: Dave Beckwith - Westminster, Colorado, USA, dbeck88 [at] gmail dot com
+*  Copyleft 2016 - no rights reserved
+***************************************************************************/
